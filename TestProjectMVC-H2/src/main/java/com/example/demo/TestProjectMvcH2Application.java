81cf34c2df620b4b081b3ab1fb33b5343e736048
eDelaMora <eDelaMora@CLQL001412>
2019-11-20 00:37:11 -0600

    create master branch initial project

---
+++ TestProjectMVC-H2/src/main/java/com/example/demo/TestProjectMvcH2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectMvcH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectMvcH2Application.class, args);
+	}
+
+}
