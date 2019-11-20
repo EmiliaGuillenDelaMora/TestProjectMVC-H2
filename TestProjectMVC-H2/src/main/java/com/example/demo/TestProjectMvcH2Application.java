fd82e6fa22aee57880258c6c636d5fef64e2a99f
eDelaMora <eDelaMora@CLQL001412>
2019-11-20 00:57:00 -0600

    create mvc structure

--- TestProjectMVC-H2/src/main/java/com/example/demo/TestProjectMvcH2Application.java
+++ TestProjectMVC-H2/src/main/java/com/example/demo/TestProjectMvcH2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestProjectMvcH2Application {
 
 	public static void main(String[] args) {
+		 /* Responsible for launching the boot application. */
 		SpringApplication.run(TestProjectMvcH2Application.class, args);
 	}
 
